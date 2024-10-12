048b709975318921188ef00aa3a1977257cb853f
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-10-13 01:22:14 +0700

    feat: shorten address

--- frontend/components/Account.tsx
+++ frontend/components/Account.tsx
@@ -8,7 +8,7 @@ export function Account() {
         <button className='rounded-full bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] px-8 py-2 float-right'
                 onClick={() => disconnect()}
                 suppressHydrationWarning>
-            Disconnect Wallet
+            {address?.substring(0,6)}...{address?.substring(38,42)}
         </button>
     )
 }
